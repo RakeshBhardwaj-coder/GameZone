e1d3baf3760526bc38782ccb779aea187ff187da
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2025-02-19 00:35:58 +0530

    js check

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+const searchButton = document.getElementById('search-btn');
+
+// Now you can work with the button element:
+searchButton.addEventListener('click', function() {
+    console.log('Search button clicked!');
+    // ... your code here ...
+});
