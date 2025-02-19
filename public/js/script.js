d1c9832b5de1fda43c34adf3d39fc1417c536a07
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2025-02-20 01:16:56 +0530

    not working

--- public/js/script.js
+++ public/js/script.js
@@ -103,8 +103,8 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 window.onYouTubeIframeAPIReady = function() { // Make it a global function
 player = new YT.Player('player', {
-  height: 500, // Make player responsive
-  width: 900,  // Make player responsive
+  height: "100%", // Make player responsive
+  width: "100%",  // Make player responsive
   videoId: '',  // Initialize without a video
   playerVars: {
       playersinline: 1,
@@ -113,7 +113,8 @@ player = new YT.Player('player', {
       rel: 0, // Don't show related videos
   },
   events: {
-      'onReady': onPlayerReady
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
   }
 });
 };
